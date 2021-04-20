f7d5abb44989b2387012f915c1cf25a4608a9cbb
nadiia <nadiia.oleks@gmail.com>
2021-04-20 16:35:57 +0300

    desktop version with animation is ready

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,43 @@ $(function () {
         $(this).next().fadeIn(500,arguments.callee);
     })
 
+
+})
+
+
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.matchMedia({
+  
+    // desktop
+    "(min-width:1440px)": function() {
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".top",
+        scrub: true,
+        pin:true,
+        start: "top top",
+        end: "+=600"
+      },
+      
+})
+tl.to('.top',{
+    scale: 4
+})
+tl.to(".top__label", {
+    yPercent:-200,
+    
+},"<");
+tl.to("#top-title", {
+    yPercent:-200,
+},"<");
+
+tl.to('.top__arrow',{
+    yPercent:1000,
+},"<");
+
+tl.to('.about',{
+    y:-1000
+})
+
+}
 })
